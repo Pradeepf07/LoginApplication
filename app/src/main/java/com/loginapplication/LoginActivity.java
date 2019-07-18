c811a55f6c1df62fa5cd6156e1707ecbc1c55ec4
pradeepf07 <pradeepf07@gmail.com>
2019-07-18 14:22:14 +0530

    intent

--- app/src/main/java/com/loginapplication/LoginActivity.java
+++ app/src/main/java/com/loginapplication/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.loginapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,11 +17,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         final EditText username,password;
+        TextView signupclick ;
         Button bt1;
 
         username = findViewById(R.id.editText3);
         password = findViewById(R.id.editText4);
-
+        signupclick = findViewById(R.id.textView4);
         bt1 = findViewById(R.id.button2);
 
         bt1.setOnClickListener( new View.OnClickListener() {
@@ -38,6 +41,16 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(LoginActivity.this,"In valid User",Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+
+        signupclick.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/loginapplication/SignupActivity.java
+++ app/src/main/java/com/loginapplication/SignupActivity.java
@@ -2,6 +2,10 @@ package com.loginapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        final EditText name,dob,emailaddress;
+        final Spinner gender;
+        Button singupbtn;
+
+        name = findViewById(R.id.editText);
+        dob = findViewById(R.id.editText5);
+        gender = findViewById(R.id.spinner);
+        emailaddress = findViewById(R.id.editText6);
+
+        singupbtn = findViewById(R.id.button);
+
+        singupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(name.getText().toString().equals("")){
+
+                }
+                else if (dob.getText().toString().equals(""))
+                {
+
+                }
+                else if(gender.getSelectedItem().toString().equals("Please select Gender"))
+                {
+
+                }
+                else if(emailaddress.getText().toString().equals(""))
+                {
+
+                }
+                else {
+
+                }
+            }
+        });
     }
 }
