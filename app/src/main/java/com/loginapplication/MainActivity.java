1926e2486dff1b4e0ec57faf569b3f6313b10113
pradeepf07 <pradeepf07@gmail.com>
2019-07-18 15:41:58 +0530

    sharedpreferences

--- app/src/main/java/com/loginapplication/MainActivity.java
+++ app/src/main/java/com/loginapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.loginapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("userdata", Context.MODE_PRIVATE);
+        String loginhint = pref.getString("loginhint",null);
+
+        if(loginhint == null){
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        }
+        else {
+            Toast.makeText(MainActivity.this,loginhint,Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/loginapplication/SignupActivity.java
+++ app/src/main/java/com/loginapplication/SignupActivity.java
@@ -1,11 +1,16 @@
 package com.loginapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -17,8 +22,10 @@ public class SignupActivity extends AppCompatActivity {
 
         final EditText name,dob,emailaddress;
         final Spinner gender;
+        ImageView backarrow;
         Button singupbtn;
 
+        backarrow = findViewById(R.id.imageView3);
         name = findViewById(R.id.editText);
         dob = findViewById(R.id.editText5);
         gender = findViewById(R.id.spinner);
@@ -45,8 +52,21 @@ public class SignupActivity extends AppCompatActivity {
 
                 }
                 else {
+                    SharedPreferences pref = getApplicationContext().getSharedPreferences("userdata", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
 
+                    editor.putString("loginhint", "true");
+                    editor.commit();
+
+                    startActivity(new Intent(SignupActivity.this,MainActivity.class));
                 }
+            }
+        });
+
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 startActivity(new Intent(SignupActivity.this,LoginActivity.class));
             }
         });
     }
